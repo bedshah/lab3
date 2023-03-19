c8ab1d5e0c5064961a4740c5a7b63ece74ffc17e
bed shah <sbed19@tbc.edu.np>
2023-03-19 22:20:29 +0545

    lab3 completed

---
+++ app/src/main/java/com/example/wordapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.wordapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void launchSecondActivity(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+}
